b5e188ea2b07c528947d488d66b9a3e5a7417598
eribiy <katerina.ribiy@gmail.com>
2016-05-24 18:28:46 +0300

    kate

--- home work/base1/js/scripts.js
+++ home work/base1/js/scripts.js
@@ -25,3 +25,20 @@ $(document).ready(function(){
         ]
     });
 });
+var img1=document.getElementById("img1");
+var img2=document.getElementById("img2");
+
+img1.onclick=img2.onclick=function() {
+    if(!this.state) {
+        this.state=true;
+        var div=document.createElement("DIV");
+        div.innerHTML=this.innerHTML;
+        div.className="popDiv";
+        this.div=div;
+        document.body.appendChild(div);
+    }
+    else {
+        this.state=false;
+        document.body.removeChild(this.div);
+    }
+}
