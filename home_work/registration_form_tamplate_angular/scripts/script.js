905dc9880aa3f9edbafcd99e4aaa233a5266fcd6
eribiy <katerina.ribiy@gmail.com>
2017-03-01 16:35:10 +0200

    ang

--- home_work/registration_form_tamplate_angular/scripts/script.js
+++ home_work/registration_form_tamplate_angular/scripts/script.js
@@ -1,8 +1,9 @@
 /**
  * Created by user on 24.02.2017.
  */
-var app = angular.module("myApp", []);
-
-app.controller("validateCtrl", function($scope) {
-
+var app = angular.module("myApp", ["ngMessages"]);
+app.controller("mainCtrl", function($scope) {
+    $scope.email = "asd@asd.com"
 });
+
+
